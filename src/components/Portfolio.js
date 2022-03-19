5b12a71d3ab12e6dff1fb56e4e4173c2e4913ce8
Tony <ngount10@hotmail.com>
2022-03-18 23:36:56 -0700

    add to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,11 @@
 import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+
+//images//
 import listitimage from "../images/listitimage.png";
+import AdoptPet from "../images/AdoptPet.PNG";
+
 
 // REACT POPUPBOX
 import { PopupboxManager, PopupboxContainer } from "react-popupbox";
@@ -33,6 +37,35 @@ const openPopupboxListit = () => {
 
     }
 
+//PetApp
+const openPopupboxAdoptPet = () => {
+    const content = (
+        <>
+        <img className="portfolio-image-popupbox" src={AdoptPet} alt="AdoptPet..." />
+        <p>Project Listit is a skinny version of Facebook Marketplace / Craigslist where users can buy and sell goods. Full-stack CRUD application built with MongoDB, Express, React, Node.js, and Google Maps Embed API.</p>
+        <b>GitHub:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/tonyngoun/ipet", "_blank")}>https://github.com/tonyngoun/ipet</a>
+        <br />
+        </>
+    )
+    PopupboxManager.open({ content })
+}
+    const popupboxConfigAdoptPet = {
+        titleBar: {
+            enable: true,
+            text: "iPet Project"
+        },
+        fadeIn: true,
+        fadeInSpeed: 500
+
+
+    }
+
+    
+
+
+
+
+
 
 
     return(
@@ -40,17 +73,28 @@ const openPopupboxListit = () => {
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">portfolio</h1>
                 <div className="image-box-wrapper row justify-content-center">
+                    {/*list it */}
                     <div className="portfolio-image-box" onClick={openPopupboxListit}>
                     <img className="portfolio-image" src={listitimage} alt="List it Project..."/>
                     <div className="overflow"></div>
                     <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
                 </div>
+                {/*adopt pet */}
+                <div className="portfolio-image-box" onClick={openPopupboxAdoptPet}>
+                    <img className="portfolio-image" src={AdoptPet} alt="AdoptPet Project..."/>
+                    <div className="overflow"></div>
+                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
+                </div>
+
+
                 </div>
             
             
             </div>
 
         <PopupboxContainer {...popupboxConfigListit}/>
+        <PopupboxContainer {...popupboxConfigAdoptPet} />
+
         </div>
     )
 }                                                                                                                                                                                                                                         
